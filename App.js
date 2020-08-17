cbfc38bb681d18c558b72a51b843626885f2cef3
Sergio Ruiz Pérez <sergiotto@outlook.com>
2020-08-17 14:34:33 +0200

    hora y fecha

--- App.js
+++ App.js
@@ -9,6 +9,7 @@
 import React, {useState} from 'react';
 import {StyleSheet, Text, View, FlatList} from 'react-native';
 import Cita from './components/Cita';
+import Formulario from './components/Formulario';
 
 const App = () => {
   const [citas, setCitas] = useState([
@@ -16,14 +17,24 @@ const App = () => {
     {id: '2', paciente: 'Ana', propietario: 'Sergio', sintomas: 'No come'},
     {id: '3', paciente: 'Ana', propietario: 'Sergio', sintomas: 'No come'},
   ]);
+
+  const eliminarPaciente = (id) => {
+    setCitas((citasActuales) => citasActuales.filter((cita) => cita.id !== id));
+  };
+
   return (
     <>
       <View style={styles.contenedor}>
         <Text style={styles.titulo}>Administrador de citas</Text>
-
+        <Formulario />
+        <Text style={styles.titulo}>
+          {citas.length > 0 ? 'Administra tus citas' : 'No hay citas'}
+        </Text>
         <FlatList
           data={citas}
-          renderItem={({item}) => <Cita item={item} />}
+          renderItem={({item}) => (
+            <Cita item={item} eliminarPaciente={eliminarPaciente} />
+          )}
           keyExtractor={(cita) => cita.id}
         />
       </View>
@@ -40,6 +51,7 @@ const styles = StyleSheet.create({
     color: '#FFF',
     textAlign: 'center',
     marginTop: 40,
+    marginBottom: 20,
     fontSize: 24,
     fontWeight: 'bold',
   },
--- components/Cita.js
+++ components/Cita.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, TouchableHighlight} from 'react-native';
 
-const Cita = ({item}) => {
+const Cita = ({item, eliminarPaciente}) => {
+  const dialogoEliminar = (id) => {
+    console.log('eliminando...', id);
+    eliminarPaciente(id);
+  };
   return (
     <View style={styles.cita}>
       <View>
@@ -15,6 +19,13 @@ const Cita = ({item}) => {
       <View>
         <Text style={styles.label}>Sintomas:</Text>
         <Text style={styles.texto}>{item.sintomas}</Text>
+      </View>
+      <View>
+        <TouchableHighlight
+          onPress={() => dialogoEliminar(item.id)}
+          style={styles.btnEliminar}>
+          <Text style={styles.textoEliminar}>Eliminar &times;</Text>
+        </TouchableHighlight>
       </View>
     </View>
   );
@@ -36,6 +47,17 @@ const styles = StyleSheet.create({
   },
   texto: {
     fontSize: 18,
+  },
+  btnEliminar: {
+    padding: 10,
+    backgroundColor: 'red',
+    marginVertical: 10,
+  },
+  textoEliminar: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
 export default Cita;
