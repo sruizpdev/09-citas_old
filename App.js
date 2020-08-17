c48e9078459ab2a2f71792076c8d4684205dcf71
Sergio Ruiz Pérez <sergiotto@outlook.com>
2020-08-17 09:58:27 +0200

    new app

--- App.js
+++ App.js
@@ -1,68 +1,114 @@
-import React, {useState} from 'react';
-import {Text, StyleSheet, View, FlatList} from 'react-native';
-import Cita from './components/Cita';
-import Formulario from './components/Formulario';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
 
-const App = () => {
-  const [citas, setCitas] = useState([
-    {
-      id: '1',
-      paciente: 'Hook',
-      propietario: 'Juan',
-      sintomas: 'No come',
-    },
-    {
-      id: '2',
-      paciente: 'Redux',
-      propietario: 'Ana',
-      sintomas: 'No duerme',
-    },
-    {
-      id: '3',
-      paciente: 'Native',
-      propietario: 'Sergio',
-      sintomas: 'No canta',
-    },
-  ]);
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
 
-  const eliminarPaciente = id => {
-    setCitas(citasActuales => {
-      return citasActuales.filter(cita => cita.id !== id);
-    });
-  };
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
 
+const App: () => React$Node = () => {
   return (
     <>
-      <View style={styles.contenedor}>
-        <Text style={styles.titulo}>Administrador de citas</Text>
-        <Formulario />
-        <Text style={styles.titulo}>
-          {citas.length > 0 ? 'Administra tus citas' : 'No hay citas'}
-        </Text>
-
-        <FlatList
-          data={citas}
-          renderItem={({item}) => (
-            <Cita item={item} eliminarPaciente={eliminarPaciente} />
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}>
+          <Header />
+          {global.HermesInternal == null ? null : (
+            <View style={styles.engine}>
+              <Text style={styles.footer}>Engine: Hermes</Text>
+            </View>
           )}
-          keyExtractor={cita => cita.id}
-        />
-      </View>
+          <View style={styles.body}>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Step One</Text>
+              <Text style={styles.sectionDescription}>
+                Edit <Text style={styles.highlight}>App.js</Text> to change this
+                screen and then come back to see your edits.
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>See Your Changes</Text>
+              <Text style={styles.sectionDescription}>
+                <ReloadInstructions />
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Debug</Text>
+              <Text style={styles.sectionDescription}>
+                <DebugInstructions />
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Learn More</Text>
+              <Text style={styles.sectionDescription}>
+                Read the docs to discover what to do next:
+              </Text>
+            </View>
+            <LearnMoreLinks />
+          </View>
+        </ScrollView>
+      </SafeAreaView>
     </>
   );
 };
+
 const styles = StyleSheet.create({
-  contenedor: {
-    backgroundColor: '#AA076B',
-    flex: 1,
+  scrollView: {
+    backgroundColor: Colors.lighter,
   },
-  titulo: {
-    marginTop: 40,
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
     fontSize: 24,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    color: '#FFF',
-    marginBottom: 10,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
   },
 });
+
 export default App;
